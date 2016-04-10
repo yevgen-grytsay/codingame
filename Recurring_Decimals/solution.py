072cac4e8cd92602e7fb029e925c3f69f02fdff0
Yevgen Grytsay <yevgen_grytsay@mail.ru>
2016-04-10 12:14:40 +0300

    Решение готово.

--- Recurring_Decimals/solution.py
+++ Recurring_Decimals/solution.py
@@ -1,24 +1,11 @@
-import sys
-import math
-
-# Auto-generated code below aims at helping you parse
-# the standard input according to the problem statement.
-
-# n = int(raw_input())
-n = 2
-n = 7
-n = 195312500
-n = 561
-# n = 210
+n = int(raw_input())
 
 dividend = 1
-dividend *= 10
 fraction_parts = []
 pairs = []
-
 while True:
     zeroes = []
-    # pair_candidates = [(dividend, n)]
+    pair = (dividend, n)
     if n > dividend:
         fraction_parts.append('0')
     else:
@@ -29,23 +16,15 @@ while True:
         if dividend == 0:
             break
 
-    pair = (dividend, n)
     if pair in pairs:
+        index = pairs.index(pair)
         fraction_parts = fraction_parts[:-1]
-        fraction_parts.insert(0, '(')
+        fraction_parts.insert(index, '(')
         fraction_parts.append(')')
         break
 
-    # if len(zeroes):
-    #     pairs.append(pair_candidate)
-
     pairs.append(pair)
     dividend *= 10
-# quotient
-# reminder
 
-
-# Write an action using print
-# To debug: print >> sys.stderr, "Debug messages..."
-
-print '0.' + ''.join(fraction_parts)
+fraction_parts.insert(1, '.')
+print ''.join(fraction_parts)
