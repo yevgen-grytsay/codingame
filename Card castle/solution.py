440dc07d8dc6550fce83d8f48038f95fb914d20e
Yevgen Grytsay <yevgen_grytsay@mail.ru>
2016-12-18 00:35:43 +0200

    Проходит все тесты, кроме "Shifted"

---
+++ Card castle/solution.py
@@ -0,0 +1,20 @@
+h = int(raw_input())
+lines = []
+for i in xrange(h):
+    s = raw_input()
+    lines.append('.'+s+'.')
+
+castle = "\n".join(lines)
+castle_rot = "\n".join(map(lambda t: "".join(t), zip(*lines)))
+if castle.find('.\\') > -1 \
+        or castle.find('/.') > -1 \
+        or castle.find('/\./\\') > -1 \
+        or castle.find('//') > -1 \
+        or castle.find('\\\\') > -1 \
+        or castle_rot.find('\.') > -1 \
+        or castle_rot.find('/.') > -1 \
+        or castle_rot.find('//') > -1 \
+        or castle_rot.find('\\\\') > -1:
+    print 'UNSTABLE'
+else:
+    print 'STABLE'
