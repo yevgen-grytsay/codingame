050632f27a1558601253910ef95c3f53540386bd
Yevgen Grytsay <yevgen_grytsay@mail.ru>
2016-12-17 20:45:16 +0200

    Все символы, кроме скобок, убираются из строки

--- Brackets_Enhanced_Edition/solution.py
+++ Brackets_Enhanced_Edition/solution.py
@@ -14,6 +14,10 @@ class Abc(object):
         counterparts[b] = a
 
     @classmethod
+    def filter_brackets(cls, char):
+        return char in cls.brackets
+
+    @classmethod
     def is_bracket(cls, char):
         return char in cls.brackets
 
@@ -38,32 +42,32 @@ def search(chars, context, bracket_context=None):
         if c.isalnum():
             continue
 
-        if c in Abc.brackets:
-            if bracket_context is None:
+        if bracket_context is None:
+            bracket_context = Abc.to_opening(c)
+            context.append(bracket_context)
+            continue
+        else:
+            if Abc.is_same(bracket_context, c):
+                # Смотреть оставшиеся символы и отсекать заведомо неверные решения
+                op = Abc.to_opening(c)
+                cs = Abc.flip(op)
+                if count_matching(chars, c) % 2 == 0:
+                    c = cs
+                else:
+                    c = first([
+                        (op, functools.partial(search, chars[:], context[:] + [op], op)),
+                        (cs, functools.partial(search, chars[:], context[:-1], prev_context(context)))
+                    ])
+                if c == op:
+                    bracket_context = c
+                    context.append(c)
+                else:
+                    bracket_context = prev_context(context)
+                    context.pop()
+            else:
                 bracket_context = Abc.to_opening(c)
                 context.append(bracket_context)
-                continue
-            else:
-                if Abc.is_same(bracket_context, c):
-                    # Смотреть оставшиеся символы и отсекать заведомо неверные решения
-                    op = Abc.to_opening(c)
-                    cs = Abc.flip(op)
-                    if count_matching(chars, c) % 2 == 0:
-                        c = cs
-                    else:
-                        c = first([
-                            (op, functools.partial(search, chars[:], context[:] + [op], op)),
-                            (cs, functools.partial(search, chars[:], context[:-1], prev_context(context)))
-                        ])
-                    if c == op:
-                        bracket_context = c
-                        context.append(c)
-                    else:
-                        bracket_context = prev_context(context)
-                        context.pop()
-                else:
-                    bracket_context = Abc.to_opening(c)
-                    context.append(bracket_context)
+
     if len(context) > 0:
         raise Exception("Wrong")
     return True
@@ -99,7 +103,8 @@ def count_matching(chars, bracket):
 n = int(raw_input())
 lines = []
 for i in xrange(n):
-    lines.append(raw_input())
+    line = filter(Abc.filter_brackets, list(raw_input()))
+    lines.append("".join(line))
 for i in xrange(n):
     try:
         search(list(lines[i]), [])
