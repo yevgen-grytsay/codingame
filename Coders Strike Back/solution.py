20de82d534065c3845afd8a4a89bab0772389366
Yevgen Grytsay <yevgen_grytsay@mail.ru>
2016-12-27 12:43:36 +0200

    Бронзовая лига

--- Coders Strike Back/solution.py
+++ Coders Strike Back/solution.py
@@ -1,4 +1,29 @@
 import sys
+from math import sqrt, ceil
+
+
+def calc_distance(a, b):
+    dx = b[0] - a[0]
+    dy = b[1] - a[1]
+    return sqrt(pow(dx, 2) + pow(dy, 2))
+
+
+def cross_circle(a, b, radius):
+    dx = b[0] - a[0]
+    dy = b[1] - a[1]
+    r = sqrt(pow(dx, 2) + pow(dy, 2))
+
+    ccos = dx / r
+    ssin = dy / r
+
+    return int(ceil(b[0] - ccos * radius)), int(ceil(b[1] - ssin * radius))
+
+
+def get_thrust(distance):
+    if distance > 100:
+        return 100
+    else:
+        return int(round(distance))
 
 # game loop
 while True:
@@ -12,17 +37,21 @@ while True:
 
     # Write an action using print
     # To debug: print >> sys.stderr, "Debug messages..."
-    # print >> sys.stderr, next_checkpoint_dist
+    if next_checkpoint_dist > 700:
+        next_x, next_y = cross_circle((x, y), (next_checkpoint_x, next_checkpoint_y), 700)
+    else:
+        next_x, next_y = cross_circle((x, y), (next_checkpoint_x, next_checkpoint_y), 600)
+    print >> sys.stderr, (next_checkpoint_x, next_checkpoint_y), (next_x, next_y)
 
     boost_used = False
-    thrust = 100
+    thrust = get_thrust(calc_distance((x, y), (next_x, next_y)))
     if abs(next_checkpoint_angle) > 90:
         thrust = 0
-    elif boost_used and next_checkpoint_dist > 10000:
+    elif boost_used and next_checkpoint_dist > 5000:
         thrust = "BOOST"
         boost_used = True
 
     # You have to output the target position
     # followed by the power (0 <= thrust <= 100)
     # i.e.: "x y thrust"
-    print str(next_checkpoint_x) + " " + str(next_checkpoint_y) + " " + str(thrust)
+    print str(next_x) + " " + str(next_y) + " " + str(thrust)
